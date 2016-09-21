a85f7d235300aa4b98548112ee3f84799fa5d849
marb <marb61a@gmail.com>
2016-09-21 16:52:59 +0000

    added facebook auth

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-var UserSchema = new mongoose.Schema({
+var UserSchema = new Schema({
     email : {type : String, unique : true, lowercase : true},
+    facebook : String,
+    token : Array,
     profile : {
         name : {type : String, default : ''},
         picture : {type : String, default : ''},
