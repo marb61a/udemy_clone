fede853f8b1b6a4c71d78a78337cd05a56d9686b
marb <marb61a@gmail.com>
2016-10-06 17:40:58 +0000

    updated main routes

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,16 @@
+var Course = require("../models/course");
+var User = require('../models/user');
+
+var async = require('async');
+
 module.exports = function(app){
     app.get('/', function(req, res, next){
         res.render('main/home');
-    });    
+    });
+    
+    app.get('/courses', function(req, res, next){
+        Course.find({}, function(err, courses){
+            res.render('courses/courses', { courses: courses });
+        });
+    });
 };
