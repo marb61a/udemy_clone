17d3c73a5e94d9fc748776d994653b4ffca3fba0
marb <marb61a@gmail.com>
2016-10-07 13:16:00 +0000

    updated main js with call back functions

--- routes/main.js
+++ routes/main.js
@@ -16,6 +16,29 @@ module.exports = function(app){
     
     app.get('/courses/:id', function(req, res, next) {
         async.parallel([
+            function(callback){
+                Course.findOne({_id: req.params.id})
+                    .populate('ownByStudent.user')
+                    .exec(function(err, foundCourse){
+                        callback(err, foundCourse);
+                    });
+            },
+            
+            function(callback){
+                User.findOne({_id: req.user._id, 'coursesTaken.course': req.params.id})
+                    .populate('coursesTaken.course')
+                    .exc(function(err, foundUserCourse){
+                        callback(err, foundUserCourse);
+                    });
+            },
+            
+            function(callback) {
+                User.findOne({ _id: req.user._id, 'coursesTeach.course': req.params.id})
+                    .populate('coursesTeach.course')
+                    .exec(function(err, foundUserCourse) {
+                        callback(err, foundUserCourse);
+                    });
+            }
             
         ]);
     });
