3b9829023dbb098527e9b7134616217b42561594
marb <marb61a@gmail.com>
2016-10-13 17:47:56 +0000

    updated payment routes

--- routes/payment.js
+++ routes/payment.js
@@ -13,10 +13,42 @@ module.exports = function(app){
         async.waterfall([
             function(callback){
                 Course.findOne({ _id: courseId }, function(err, foundCourse){
-                    if(foundCourse)
+                    if(foundCourse){
                         callback(err, foundCourse);
+                    }
                 });
-            }    
+            },
+            
+            function(foundCourse, callback){
+                stripe.customers.create({
+                    source: stripeToken,
+                    email: req.user.email
+                }).then(function(customer){
+                    return stripe.charges.create({
+                        amount : foundCourse.price,
+                        currency : 'usd',
+                        customer : customer.id
+                    }).then(function(charge){
+                        async.parallel([
+                            function(callback){
+                                Course.update({
+                                    _id: courseId,
+                                    'ownByStudent.user': { $ne: req.user._id }
+                                },
+                                {
+                                    $push: { ownByStudent: { user: req.user._id }},
+                                    $inc: { totalStudents: 1 }
+                                }, function(err, count){
+                                    if(err)
+                                        return next(err);
+                                    callback(err);
+                                }
+                                );
+                            }    
+                        ]);
+                    });
+                });
+            }
         ]);
     });
 };
